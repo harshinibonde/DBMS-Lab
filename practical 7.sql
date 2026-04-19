0a98f0dc06081c263918e37b83a799bb0c4c0da9
Harshini Bonde <harshinibonde07@gmail.com>
2026-04-20 01:32:18 +0530

    Add procedures to sum first 10 numbers and evens

---
+++ practical 7.sql
@@ -0,0 +1,35 @@
+CREATE OR REPLACE PROCEDURE SumFirst10Numbers_WhileLoop AS
+    i NUMBER := 1;
+    sum1 NUMBER := 0;
+BEGIN
+    WHILE i <= 10 LOOP
+        sum1 := sum1 + i;
+        i := i + 1;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('Sum of first 10 numbers using WHILE loop: ' || sum1);
+END;
+/
+
+BEGIN
+    SumFirst10Numbers_WhileLoop;
+END;
+    /
+
+
+
+
+CREATE OR REPLACE PROCEDURE SumFirst10EvenNumbers_ForLoop AS
+    sum1 NUMBER := 0;
+BEGIN
+    FOR i IN 1..10 LOOP
+        sum1 := sum1 + (2 * i);
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('Sum of first 10 even numbers using FOR loop: ' || sum1);
+END;
+/
+
+BEGIN
+        SumFirst10EvenNumbers_ForLoop;
+    END;
+    /
+
